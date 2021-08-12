30b197d2afc64fb0bb4e0ac9373bf6f096382f6e
katderv <kat.derv98@gmail.com>
2021-08-12 03:38:52 +0300

    first try of indexing

--- hars.php
+++ hars.php
@@ -12,19 +12,61 @@ require_once 'vendor/autoload.php';
  use \JsonMachine\JsonMachine;
  use JsonMachine\JsonDecoder\PassThruDecoder;
 
- $fruits = JsonMachine::fromFile("techsetupgear.wordpress.com_Archive-21-07-23-21-24-35.har",'/log/entries', new PassThruDecoder);
- foreach ($fruits as $fruit){
-   foreach (JsonMachine::fromString($fruit, "/startedDateTime") as $data){
+ /*
+ $insertRecordQuery = "INSERT INTO har_file VALUES(null,'user1@email.com','cosmote')";
+
+ $result = $connection->query("SELECT MAX(id_har) FROM har_file WHERE user_email = 'user1@email.com';");
+
+ if ($result->num_rows > 0) {
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+     $har_id = $row["id_har"];
+   }
+ }
+
+ 
+*/
+
+ $entries = JsonMachine::fromFile("techsetupgear.wordpress.com_Archive-21-07-23-21-24-35.har",'/log/entries', new PassThruDecoder);
+ foreach ($entries as $entry){
+  $x = 1;
+   foreach (JsonMachine::fromString($entry, "") as $data){
+    if($x === 2){
+      //first item
+      echo $data; // output: dog
+      
+    }
+    if($x === 3){
+      $y = 1;
+      foreach ($data as &$value) {
+        if ($y === 5){
+          foreach ($value as &$in) {
+            foreach ($in as &$inn) {
+              echo $inn;
+          }
+        }
+        }
+        $y++;
+      }
+    }
+    if($x === 9){
+      //first item
+      echo $data; // output: dog
+      echo "end foreach";
+    }
+    $x++;
    //print_r($startedDateTime);
-    $date = new DateTime($data);
-    $new_data = date_format($date,'Y-m-d H:i:s');
-    $insertRecordQuery = "INSERT INTO hars VALUES('".$new_data."')";
+    //$date = new DateTime($data);
+    //$new_data = date_format($date,'Y-m-d H:i:s');
+    /*
+    $insertRecordQuery = "INSERT INTO entries VALUES('".$new_data."')";
     if(mysqli_query($connection, $insertRecordQuery)){
        echo "success";
     }
     else{
        echo "error:".mysqli_error($connection);
     }
+    */
   }
  }
 
