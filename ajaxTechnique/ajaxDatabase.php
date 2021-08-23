8ac283677e6e076ac0fb2a6124c2e24af95033cd
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-08-23 20:39:17 +0300

    setted the ajax in my db

--- ajaxTechnique/ajaxDatabase.php
+++ ajaxTechnique/ajaxDatabase.php
@@ -17,7 +17,7 @@
             $("#btn").click(function(){
                 console.log("Success");
                 commentsCount += 2;
-                $("#comments").load("load-comments.php", {
+                $("#comments").load("loadFields.php", {
                     commentsNewCount: commentsCount
                 });
             });
@@ -28,14 +28,14 @@
 <body>
     <div id="comments">
         <?php 
-            $sql = "SELECT * FROM comments LIMIT 2";
+            $sql = "SELECT id_entry,value FROM header LIMIT 2";
             $result = mysqli_query($conn, $sql);
             if(mysqli_num_rows($result) > 0){
                 while($row = mysqli_fetch_assoc($result)){
                     echo "<p>";
-                    echo $row['author'];
+                    echo $row['id_entry'];
                     echo "<br>";
-                    echo $row['message'];
+                    echo $row['value'];
                     echo "</p>";
 
                 }
--- ajaxTechnique/loadFields.php
+++ ajaxTechnique/loadFields.php
@@ -1,20 +1,20 @@
 <?php 
     include 'dbh.php';
 
-    $commentNewCount = $_POST['commentsNewCount'];
+    $commentsNewCount = $_POST['commentsNewCount'];
 
-    $sql = "SELECT * FROM comments LIMIT $commentNewCount";
+    $sql = "SELECT id_entry,value FROM header LIMIT $commentsNewCount";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_assoc($result)){
             echo "<p>";
-            echo $row['author'];
+            echo $row['id_entry'];
             echo "<br>";
-            echo $row['message'];
+            echo $row['value'];
             echo "</p>";
 
         }
     }else{
-        echo "There are no comments!";
+        echo 'There are no comments!';
     }
 ?>
