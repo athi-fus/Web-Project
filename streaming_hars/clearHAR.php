23522f388303869b4dd7fe125adab9b972465970
katderv <kat.derv98@gmail.com>
2021-08-25 21:51:25 +0300

    cleaning almost ready

--- streaming_hars/clearHAR.php
+++ streaming_hars/clearHAR.php
@@ -26,21 +26,84 @@ foreach($objects as $object){
                         unset($object->$key->$val);
                     }
                     if($val == "headers"){
+                        $x = 0;
                         foreach($thi as $valu => $inn){
                             if(strcasecmp($inn->{"name"}, 'Host') == 0 or strcasecmp($inn->{"name"}, 'content-type') == 0 or strcasecmp($inn->{"name"}, 'cache-control') == 0 or strcasecmp($inn->{"name"}, 'pragma') == 0 or strcasecmp($inn->{"name"}, 'expires') == 0 or strcasecmp($inn->{"name"}, 'age') == 0 or strcasecmp($inn->{"name"}, 'last-modified') == 0){
-                                echo $inn->{"name"};
+                                $x++;
                                 continue;
                                 
                             }
                             else{
-                                echo "in else";
-                                unset($object->$key->$val->$inn);
+                                unset($object->$key->$val[$x]);
+                                $x++;
                             }
                
                         }
+                        
+                    }
+                    if($val == "cookies"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "queryString"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "headersSize"){
+                        unset($object->$key->$val);
                     }
                 }
             }
+            if ($key == "response") {
+                foreach($value as $val => $thi){
+                    if($val == "httpVersion"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "headers"){
+                        $x = 0;
+                        foreach($thi as $valu => $inn){
+                            if(strcasecmp($inn->{"name"}, 'Host') == 0 or strcasecmp($inn->{"name"}, 'content-type') == 0 or strcasecmp($inn->{"name"}, 'cache-control') == 0 or strcasecmp($inn->{"name"}, 'pragma') == 0 or strcasecmp($inn->{"name"}, 'expires') == 0 or strcasecmp($inn->{"name"}, 'age') == 0 or strcasecmp($inn->{"name"}, 'last-modified') == 0){
+                                $x++;
+                                continue;
+                                
+                            }
+                            else{
+                                unset($object->$key->$val[$x]);
+                                $x++;
+                            }
+               
+                        }
+                        
+                    }
+                    if($val == "cookies"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "content"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "redirectURL"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "headersSize"){
+                        unset($object->$key->$val);
+                    }
+                    if($val == "bodySize"){
+                        unset($object->$key->$val);
+                    }
+                }
+            }
+            if ($key == "cache") {
+                unset($object->{$key});
+            }
+            if ($key == "time") {
+                unset($object->{$key});
+            }
+            if ($key == "_securityState") {
+                unset($object->{$key});
+            }
+            if ($key == "connection") {
+                unset($object->{$key});
+            }
+
+
     
 }
 fwrite($fp, json_encode((array)$object));
