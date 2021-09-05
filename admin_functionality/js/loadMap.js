c5fade708f40de7ca5950e3b30a3150171ea760b
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-09-05 21:07:16 +0300

    finished map

--- admin_functionality/js/loadMap.js
+++ admin_functionality/js/loadMap.js
@@ -1,4 +1,4 @@
-const mymap = L.map('mapid').setView([37.983810, 23.727539], 5); //Latitude, longitude, zoom level 
+const mymap = L.map('mapid').setView([37.983810, 23.727539], 6); //Latitude, longitude, zoom level 
 
 var marker = L.marker([0,0]);
 
@@ -20,13 +20,33 @@ function loadLatLong(){
     uReq.open("get", "sql/loadLatLong.php", true);
     uReq.onload = function() {
         const counter = JSON.parse(this.responseText);
-        console.log(counter);
-        for(var i=1; i<counter.length; i++){
-             marker = new L.marker([counter[i].latitude,counter[i].longitude]).addTo(mymap);
-            console.log(marker);
+        var latlngs = [];
+        var userIcon = new L.Icon({
+            iconUrl: 'imgs/marker.png',
+            iconSize: [40, 40],
+            popupAnchor: [1, -16],
+        });
+        
+        //Its important to change the number 1 with 0, i put one for testing purposes!!!!!
+        for(var i=1; i<counter.length; i++){ 
+            markerServer = new L.marker([counter[i].latitude,counter[i].longitude]).addTo(mymap).bindPopup('Marker for Server Ips');
+            markerUser = new L.marker([counter[i].lat,counter[i].lon],{icon: userIcon}).addTo(mymap).bindPopup('Marker for User Ips');
+            latlngs.push([[counter[i].latitude,counter[i].longitude],[counter[i].lat,counter[i].lon]]);
+            var thickness;
+            if(counter[i].records < 2){
+               thickness = 0.25;
+            }else if(counter[i].records >= 2 && counter[i].records < 6){
+                thickness = 0.5;
+            }else if(counter[i].records >= 6 && counter[i].records < 10){
+                thickness = 1;
+            }else{
+                thickness = counter[i].records/10; //Each time see the records and divide them by 10. ex 20 records = 2 thickness
+            }
             
+            L.polyline(latlngs, {color: 'black', weight: thickness}).addTo(mymap); //Create the polylines for each pair of userIps and ServerIps
             
         }
+        console.log(latlngs);
     };
     uReq.send();
 }
--- admin_functionality/sql/loadLatLong.php
+++ admin_functionality/sql/loadLatLong.php
@@ -1,7 +1,11 @@
 <?php 
     include 'dbh.php';
     $ips = array();
-    $sql = "SELECT longitude,latitude,COUNT(*) AS records FROM entries GROUP BY longitude ORDER BY records DESC";
+    $sql = "SELECT e.longitude,e.latitude,COUNT(e.id_entry) AS records,hr.lon,hr.lat
+    FROM entries as e
+    INNER JOIN har_file as hr ON e.id_har = hr.id_har 
+    GROUP BY e.longitude 
+    ORDER BY records DESC";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_assoc($result)){
