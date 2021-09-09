2681d58fca533f222261cb42f2aaedbcdf66e852
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-09-09 17:25:06 +0300

    completed first query of 3rd

--- admin_functionality/js/chartTTL.js
+++ admin_functionality/js/chartTTL.js
@@ -56,24 +56,24 @@ $("#submit").click(function(){
                 return false;
             }  
         }
-        // var isp_box = document.getElementsByClassName('isp-box');
-        // for(var k=1; k<isp_box.length; k++){
-        //     if(isp_box[k].checked == true){
-        //         isps.push(isp_box[k].value);
-        //     }else if(!(checked = $(".isp-checkbox-wrapper > input[type=checkbox]:checked").length)){
-        //         alert("You must check at least one checkbox for ISPs.");
-        //         return false;
-        //     } 
-        // }
+        var isp_box = document.getElementsByClassName('isp-box');
+        for(var k=1; k<isp_box.length; k++){
+            if(isp_box[k].checked == true){
+                isps.push(isp_box[k].value);
+            }else if(!(checked = $(".isp-checkbox-wrapper > input[type=checkbox]:checked").length)){
+                alert("You must check at least one checkbox for ISPs.");
+                return false;
+            } 
+        }
         content_types = JSON.stringify(content_types).replace(/\[/g, "(").replace(/\]/g, ")");
-        // isps = JSON.stringify(isps).replace(/\[/g, "(").replace(/\]/g, ")");
+        isps = JSON.stringify(isps).replace(/\[/g, "(").replace(/\]/g, ")");
 
         $.ajax({
             url:"sql/loadTTL.php",
             method: "POST",
             data:{
                 content_types: content_types,
-                // isps: isps
+                isps: isps
             },
             success: function(res){
                 // Get the data from php file and assign then into a variable
@@ -82,46 +82,58 @@ $("#submit").click(function(){
 
                 // Create arrays that gonna store information from database.
                 var maxAges = [];
+                var maxAgesUnsorted = [];
                 var step_labels = [];
                 var final_labels = [];
-                var labelData = [];
+                var final_data = new Array(10).fill(0);
                 var records = [];
-                var testAges = [];
+               
+
+                for(var k=0;k<res.length; k++){
+                    records.push(parseInt(res[k].Records));
+                }
 
                 for(var i=0; i<res.length; i++){
                     maxAges.push(parseInt(res[i].MaxAge.match(/\d+/)));
+                    maxAgesUnsorted.push(parseInt(res[i].MaxAge.match(/\d+/)));
                     
                 }
+                console.log(maxAgesUnsorted);
                 maxAges.sort((a,b)=>a-b);
-                console.log(maxAges);
-                firstAge = maxAges[0];
-                lastAge = maxAges[maxAges.length-1];
+                if(maxAges.length == 1){
+                    firstAge = 0;
+                    lastAge = maxAges[0];
+                }else{
+                    firstAge = maxAges[0];
+                    lastAge = maxAges[maxAges.length-1];
+                }
+                
                 var step = (lastAge - firstAge)/10 ;
 
                 for(var j = 0; j<=10; j++){
-                    step_labels.push(String(firstAge+j*step));
+                    step_labels.push(firstAge+j*step);        
                 }
+                console.log(step_labels);
 
                 for(var v=0; v<10; v++){
-                    final_labels.push([step_labels[v].concat('-',step_labels[v+1])]);
-                }
-                console.log(final_labels);
-
-                // Assign the proper values from database
-                for(var w=0; w<10 ; w++){  
-                    for(var l=0; l<res.length; l++){
-                        testAges.push(res[l].MaxAge);
-                        records.push(res[l].Records);    
-                        if( testAges[l].substring(8,13) == final_labels[w][0].substring(9,15)){
-                            labelData[w] = records[l];
-                            break;
-                        }else{
-                            labelData[w] = 0;
-                        }
+                    if(v == 9){
+                        final_labels.push(step_labels[v]+'-'+(step_labels[v+1]));
+                    }else{
+                        final_labels.push(step_labels[v]+'-'+(step_labels[v+1]-1));
                     }
                     
                 }
-                console.log(records);
+                console.log(final_labels);
+
+                for(var p=0; p<maxAgesUnsorted.length; p++){
+                    for(var h=0; h<10; h++){
+                        if(step_labels[h] <= maxAgesUnsorted[p] && maxAgesUnsorted[p] <= step_labels[h+1] ){
+                           final_data[h] += records[p];
+                        }
+                    }
+                }
+                console.log(final_data);
+
 
                 // Create graph and assign the proper values on it
                 const ctx = document.getElementById('myChart2').getContext('2d');
@@ -130,7 +142,7 @@ $("#submit").click(function(){
                     data: {
                         labels: final_labels,
                         datasets: [{
-                            data: labelData,
+                            data: final_data,
                             backgroundColor: '#9bc5c3',
                             borderWidth: 1,
                             barThickness: 20,
@@ -146,7 +158,7 @@ $("#submit").click(function(){
                             },
                             title: {
                                     display: true,
-                                    text: 'Average Response Time per Hour',
+                                    text: 'TTL per Content Type',
                                     color: '#9bc5c3'
                                 }
                         },
--- admin_functionality/sql/loadTTL.php
+++ admin_functionality/sql/loadTTL.php
@@ -3,17 +3,21 @@
     include 'dbh.php';
 
     $content_types = $_POST['content_types'];
-    // $isps = $_POST['isps'];
+    $isps = $_POST['isps'];
 
 
     $total = array();
     $sql = 
-    "SELECT  B.value AS MaxAge, COUNT(*) as Records 
-    FROM header A, header B 
-    WHERE A.id_entry = B.id_entry 
-    AND A.name LIKE 'content-type' 
-    AND B.value LIKE '%max-age%' 
+    "SELECT  B.value AS MaxAge, COUNT(*) as Records
+    FROM header A
+    INNER JOIN header B ON A.id_entry = B.id_entry
+    INNER JOIN entries eA ON eA.id_entry = A.id_entry 
+    INNER JOIN entries eB ON eB.id_entry = B.id_entry
+    INNER JOIN har_file hf  ON eA.id_har = hf.id_har
+    WHERE A.name LIKE 'content-type' 
     AND A.value IN $content_types
+    AND B.value LIKE '%max-age%' 
+    AND hf.provider IN $isps
     GROUP BY A.value,B.value";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) > 0){
