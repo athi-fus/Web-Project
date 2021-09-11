e034fa426f08f1171c7ad49969e71b75a1bc572f
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-09-11 20:36:15 +0300

    heatmap correction

--- user_functionality/for_heatmap_test.js
+++ user_functionality/for_heatmap_test.js
@@ -1,7 +1,7 @@
-var mymap = L.map('mapid').setView([38.272689, 21.621094], 13);
+var mymap = L.map('mapid').setView([38.272689, 21.621094], 3);
 
-var marker = L.marker([38.107547, 21.502991]).addTo(mymap);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+var marker = L.marker([38.107547, 21.502991]);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 var  baselayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
