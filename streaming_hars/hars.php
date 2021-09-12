a0395316b6f03733c4a6da2f18c247c21f240517
katderv <kat.derv98@gmail.com>
2021-09-12 15:16:24 +0300

    Update hars.php

--- streaming_hars/hars.php
+++ streaming_hars/hars.php
@@ -237,14 +237,8 @@ else{
     }
     if ($key == "serverIPAddress") {
       $serverIPAddress = $value;
-      //$response = file_get_contents('https://freegeoip.app/json/'.$value);
-     // $data = json_decode($response);
-      //$server_lat = $data->latitude;
-      //$server_lon = $data->longitude;
-
-      $ip_address = $value;
       $api_access_key = '15f55544c4b6c69056311af5e3f3ee49';
-      $ch = curl_init('http://api.ipstack.com/'.$ip_address.'?access_key=15f55544c4b6c69056311af5e3f3ee49');
+      $ch = curl_init('http://api.ipstack.com/'.$serverIPAddress.'?access_key=15f55544c4b6c69056311af5e3f3ee49');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       $response_data = curl_exec($ch);
       curl_close($ch);
